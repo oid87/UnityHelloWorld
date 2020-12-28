0c39dfe3aa1bf3adfd8ee44795ec100aff6c2154
oid87 <takahiro03270@hotmail.com>
2020-12-28 22:45:17 +0800

    add test build

---
+++ Assets/Scripts/Editor/Builder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Builder : Editor
+{
+
+    [MenuItem("Build/APK")]
+    public static void Build()
+    {
+        BuildTarget buildTarget = BuildTarget.Android;
+        // 切換到Android平台
+        EditorUserBuildSettings.SwitchActiveBuildTarget(buildTarget);
+
+        List<string> levels = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            // 獲取有效的場景
+            levels.Add(scene.path);
+        }
+
+        // 打包出 APK 名
+        string apkName = string.Format("./{0}.apk", "Test");
+        // 執行打包
+	    //string res = BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
+	    BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
+	    
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Scripts/Editor/JenkinsBuilder.cs
@@ -0,0 +1,48 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Linq;
+using UnityEditor;
+
+static class JenkinsBuilder
+{
+    [MenuItem("Auto Builder/Build Win64")]
+    public static void PerformBuildWin64()
+    {
+        var path = string.Format("Builds/Win64/{0:yyyy-MM-dd-hh-mm-ss}", DateTime.Now);
+
+        if (UnityEditorInternal.InternalEditorUtility.inBatchMode)
+        {
+            path = GetBatchOutputPath();
+
+            if (path == null)
+            {
+                throw new ArgumentException("-output is not set");
+            }
+        }
+
+        PerformBuild(path, BuildTarget.StandaloneWindows64);
+    }
+    static void PerformBuild(string path, BuildTarget target)
+    {
+        var scenes = EditorBuildSettings.scenes.Where((v) => { return v.enabled; }).Select((v) => { return v.path; });
+        BuildPipeline.BuildPlayer(scenes.ToArray(), path, target, BuildOptions.None);
+    }
+
+    static string GetBatchOutputPath()
+    {
+        var args = Environment.GetCommandLineArgs();
+        for (int ii = 0; ii < args.Length; ++ii)
+        {
+            if (args[ii].ToLower() == "-output")
+            {
+                if (ii + 1 < args.Length)
+                {
+                    return args[ii + 1];
+                }
+            }
+        }
+
+        return null;
+    }
+}
+#endif
